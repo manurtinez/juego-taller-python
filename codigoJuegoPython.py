3278a0e56a2f2265f3c7d93256f7d6f4c7db6b8b
manutim <38946479+manutim97@users.noreply.github.com>
2018-07-02 13:29:25 -0300

    cambios para insertar las imagenes

--- codigoJuegoPython.py
+++ codigoJuegoPython.py
@@ -1,6 +1,9 @@
-import pygame, sys, os
+import pygame, sys, os, glob
 from pygame.locals import *
+from PIL import Image
+from random import randint
 
+DIRIMAGENES = r"./imagenes"
 
 WHITE = (255, 255, 255)
 BLACK = (  0,   0,   0)
@@ -14,20 +17,30 @@ BRIGHTYELLOW = (255, 255,   0)
 YELLOW = (155, 155,   0)
 DARKGRAY = ( 40,  40,  40)
 
-ANCHOVENTANA = 640
-ALTOVENTANA = 480
+ANCHOVENTANA = 1280
+ALTOVENTANA = 720
+imagenes = [f for f in os.listdir(DIRIMAGENES) if os.path.splitext(f)[-1] == '.png']
 
 def main():
+	y = 100
+	x = 100
+	i = 0
 	pygame.init()
-	dibujarVentana()
-	while True:
-		chequearQuit()
-
-def dibujarVentana():
 	pantalla = pygame.display.set_mode((ANCHOVENTANA, ALTOVENTANA))
 	pygame.display.set_caption('Prueba')
 	pantalla.fill(BRIGHTGREEN)
 	pygame.display.update()
+	print(imagenes)
+	try:
+		for i in range(1,6):
+			imagen = pygame.image.load(DIRIMAGENES+os.sep+imagenes[randint(1, len(imagenes)-1)])
+			pantalla.blit(imagen, (x, y))
+			pygame.display.flip()
+			x = x+200
+	except IndexError:
+		chequearQuit()
+	while True:
+		chequearQuit()
 
 def terminate():
 	pygame.quit()
