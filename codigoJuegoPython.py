6a26eae9263d39f5f9d45b2312b1779906782888
manutim <38946479+manutim97@users.noreply.github.com>
2018-07-02 14:31:56 -0300

    solucione repeticion de imagenes

--- codigoJuegoPython.py
+++ codigoJuegoPython.py
@@ -17,9 +17,11 @@ BRIGHTYELLOW = (255, 255,   0)
 YELLOW = (155, 155,   0)
 DARKGRAY = ( 40,  40,  40)
 
-ANCHOVENTANA = 1280
-ALTOVENTANA = 720
-imagenes = [f for f in os.listdir(DIRIMAGENES) if os.path.splitext(f)[-1] == '.png']
+ANCHOVENTANA = 1600
+ALTOVENTANA = 900
+IMAGENES = [f for f in os.listdir(DIRIMAGENES) if os.path.splitext(f)[-1] == '.png']
+IMAGENESVIS = [False]*len(IMAGENES)
+
 
 def main():
 	y = 100
@@ -30,14 +32,19 @@ def main():
 	pygame.display.set_caption('Prueba')
 	pantalla.fill(BRIGHTGREEN)
 	pygame.display.update()
-	print(imagenes)
+	print(IMAGENES)
 	try:
 		for i in range(1,6):
-			imagen = pygame.image.load(DIRIMAGENES+os.sep+imagenes[randint(1, len(imagenes)-1)])
+			im = randint(1, len(IMAGENES)-1)
+			while(IMAGENESVIS[im]):
+				im = randint(1, len(IMAGENES)-1)
+			imagen = pygame.image.load(DIRIMAGENES+os.sep+IMAGENES[im])
+			IMAGENESVIS[im] = True
 			pantalla.blit(imagen, (x, y))
-			pygame.display.flip()
+			pygame.display.update()
 			x = x+200
 	except IndexError:
+		print(error)
 		chequearQuit()
 	while True:
 		chequearQuit()
