17bab5045363aeef329d91ec2b3c54b302f2f9a1
manutim <38946479+manutim97@users.noreply.github.com>
2018-07-06 11:21:30 -0300

    agregue menu de inicio

--- pruebaarrastro.py
+++ pruebaarrastro.py
@@ -1,17 +1,46 @@
 # -*- coding: utf-8 -*-
-import time
+import time, Boton
 import sys
 import pygame
 from pygame.locals import *
 from spriteImagen import *
  
+WHITE = (255, 255, 255)
+BLACK = (  0,   0,   0)
+BRIGHTRED = (255,   0,   0)
+RED = (155,   0,   0)
+BRIGHTGREEN = (  0, 255,   0)
+GREEN = (  0, 155,   0)
+BRIGHTBLUE = (  0,   0, 255)
+BLUE = (  0,   0, 155)
+BRIGHTYELLOW = (255, 255,   0)
+YELLOW = (155, 155,   0)
+DARKGRAY = ( 40,  40,  40)
  
- 
+
+
 pygame.init()
+ancho_ventana = 900
+alto_ventana = 900
+pygame.display.set_caption("Tutorial sprites Piensa 3D")
+clock = pygame.time.Clock()
 BASICFONT = pygame.font.Font('freesansbold.ttf', 18)
 WHITE     = (255, 255, 255)
- 
- 
+ANCHOBOTON=150
+ALTOBOTON=50
+ANCHOCENTROVENTANA= ancho_ventana / 2
+ALTOCENTROVENTANA= alto_ventana / 2
+FUENTEBOTON=pygame.font.SysFont("comicsansms", 25)
+screen = pygame.display.set_mode((ancho_ventana, alto_ventana))
+
+botonInicio = Boton.boton(RED, BLUE, screen, "INICIAR", ANCHOCENTROVENTANA - (ANCHOBOTON / 2),
+                            ALTOCENTROVENTANA - 30, ANCHOBOTON, ALTOBOTON, WHITE, -30, ANCHOCENTROVENTANA,
+                            ALTOCENTROVENTANA, FUENTEBOTON)
+
+botonSalir = Boton.boton(RED, BLUE, screen, "SALIR", ANCHOCENTROVENTANA - (ANCHOBOTON / 2),
+                           ALTOCENTROVENTANA + 50, ANCHOBOTON, ALTOBOTON, WHITE, 50, ANCHOCENTROVENTANA,
+                           ALTOCENTROVENTANA, FUENTEBOTON)
+
 pygame.mixer.music.set_volume(0.5)
 sonidoBien = pygame.mixer.Sound('./sonidos/109662__grunz__success.wav')
 sonidoMal = pygame.mixer.Sound('./sonidos/366107__original-sound__error_sound.wav')
@@ -27,12 +56,6 @@ def drawScore(score):
  
  
 # Definimos algunas variables que usaremos en nuestro código
- 
-ancho_ventana = 900
-alto_ventana = 900
-screen = pygame.display.set_mode((ancho_ventana, alto_ventana))
-pygame.display.set_caption("Tutorial sprites Piensa 3D")
-clock = pygame.time.Clock()
  
  
  
@@ -56,12 +79,10 @@ def correrJuego(player, player2, letra_A):
    
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
+                terminate()
             if (event.type == KEYUP):
                 if event.key == K_ESCAPE:
-                    pygame.quit()
-                    sys.exit()
+                    terminate()
                 if event.key == K_m:
                     if reproduccionMusica:
                         pygame.mixer.music.pause()
@@ -99,7 +120,38 @@ def correrJuego(player, player2, letra_A):
         pygame.display.flip()
         clock.tick(60)
        
- 
+def pantallaInicio():
+    """
+    Carga la pantalla inicial del juego
+    """
+    screen.fill(pygame.Color('dark green'))
+    while True:
+
+        events = pygame.event.get()
+        for event in events:
+            if event.type == pygame.QUIT:
+                terminate()
+
+        botonInicio.mostrarBoton()
+        botonSalir.mostrarBoton()
+
+        if botonInicio.toca(getCursorPos()) and botonIzquierdoMouseClickeado():
+            return
+        elif botonSalir.toca(getCursorPos()) and botonIzquierdoMouseClickeado():
+            terminate()
+
+        pygame.display.update()
+
+def botonIzquierdoMouseClickeado():
+    return pygame.mouse.get_pressed()[0]
+
+def getCursorPos():
+    return pygame.mouse.get_pos()
+
+def terminate():
+    pygame.quit()
+    sys.exit()
  
 if __name__ == "__main__":
+    pantallaInicio()
     main()
